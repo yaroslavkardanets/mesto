54d122f6b81adb0ae0f359de869027ead3373318
Yaroslav Kardanets <kardanets@gmail.com>
2020-09-01 20:13:56 +0300

    8.0 Вебпак.6

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: './src/index.js' ,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
@@ -33,14 +33,14 @@ module.exports = {
         loader: 'html-loader',
       },
       {
-        test: /\.(png|svg|jpg|gif|woff2)$/,
+        test: /\.(png|svg|jpg|gif|woff2|woff)$/,
         loader: 'file-loader',
       },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: './src/index.html'
     }),
     new MiniCssExtractPlugin()
   ]
