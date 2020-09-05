439898c879d1af11ef804b5add98a125f1d82fc9
Yaroslav Kardanets <kardanets@gmail.com>
2020-09-05 10:20:33 +0300

    8.0 Перед сборкой

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,30 +2,37 @@ export class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
     this._popupCloseButton = this._popup.querySelector('.popup__close-button');
+    this._handleEscClose = this._handleEscClose.bind(this);
+    this._handleOverlayClose = this._handleOverlayClose.bind(this);
     }
 
+    // Общий метод открытия всплывающих окон
     open() {
       this._popup.classList.add('popup-opened');
       this.setEventListeners();
     }
 
+    // Общий метод закрытия всплывающих окон
     close() {
       this._popup.classList.remove('popup-opened');
       this.removeEventListeners();
     }
 
-    _handleEscClose = (evt) => {
+    // Закрываем кнопкой Esc
+    _handleEscClose (evt) {
       if (evt.key === 'Escape') {
         this.close();
       }
     }
 
-    _handleOverlayClose = (evt) => {
+    // Закрываем кликом по оверлею
+    _handleOverlayClose (evt) {
       if (evt.target.classList.contains('popup')) {
         this.close();
       }
     }
 
+    // Добавляем слушатели закрытия для всплывающих окон
     setEventListeners() {
       document.addEventListener('keydown', this._handleEscClose);
       document.addEventListener('click', this._handleOverlayClose);
@@ -34,6 +41,7 @@ export class Popup {
       });
     }
 
+    // Удаляем слушатели закрытия для всплывающих окон
     removeEventListeners() {
       document.removeEventListener('keydown', this._handleEscClose);
       document.removeEventListener('click', this._handleOverlayClose);
---
+++ src/index.js
@@ -0,0 +1,116 @@
+import './pages/index.css';
+import { initialCards, Card } from './components/Card.js';
+import { FormValidator } from './components/FormValidator.js';
+import { Section } from './components/Section.js';
+import { UserInfo } from './components/UserInfo.js';
+import { PopupWithImage } from './components/PopupWithImage.js';
+import { PopupWithForm } from './components/PopupWithForm.js';
+
+// *** Переменные *** //
+// переменные profile
+const profileEditButton = document.querySelector('.profile__edit-button');
+const addCardButton = document.querySelector('.profile__add-button');
+
+// переменные popup профиля
+const nameInput = document.querySelector('.popup__input_name');
+const jobInput = document.querySelector('.popup__input_occupation');
+
+// переменные popup карточки
+const cardsElement = document.querySelector('.popup__container_cards');
+const placeInput = document.querySelector('.popup__input_cards_place');
+const imageInput = document.querySelector('.popup__input_cards_image');
+
+// переменные элементы карточек
+const elements = '.elements';
+const elementsComponent = document.querySelector(elements);
+
+
+
+// Открываем попап просмотра фотографии
+const popupWithImage = new PopupWithImage('.popup_image');
+
+const userInfo = new UserInfo({ 'title': '.profile__name', 'occupation': '.profile__occupation' });
+
+const popupUserInfo = new PopupWithForm('.popup_profile', (info) => {
+  userInfo.setUserInfo(info);
+});
+
+
+
+// Открываем всплывающее окно профиля, передаем текущие значения
+function openPopupProfile() {
+  const profileInfo = userInfo.getUserInfo();
+  nameInput.value = profileInfo.title;
+  jobInput.value = profileInfo.occupation;
+  popupUserInfo.open();
+}
+
+
+
+// *** Открываем/закрываем всплывающие окна *** //
+// Открываем всплывающее окно создания карточки
+function openPopupCards() {
+  placeInput.value = '';
+  imageInput.value = '';
+  popupNewCard.open();
+}
+
+
+
+// *** Создаем карточки *** //
+// Добавляем в разметку 6 готовых карточек
+const section = new Section({ items: initialCards, renderer: (item) => {
+  renderCard(item);} }, elements);
+
+section.renderCards();
+
+// Создаем новую карточку
+const popupNewCard = new PopupWithForm('.popup_cards', () => {
+});
+
+// Добавляем новую карточку в разметку
+function renderCard(item) {
+  const card = new Card(item, '#card-element', () => popupWithImage.open(item.link, item.name));
+  const cardElement = card.generateCard();
+  elementsComponent.prepend(cardElement);
+}
+
+// Подтверждаем создание карточки
+function cardSubmitHandler (evt) {
+  evt.preventDefault();
+  const newCard = { name: placeInput.value, link: imageInput.value };
+  renderCard(newCard);
+}
+
+
+
+// *** Валидация *** //
+// Проверяем элементы формы
+const formObjects = {
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_inactive',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active'
+}
+
+// Функция валидации
+function addValidation() {
+  const formList = Array.from(document.querySelectorAll('.popup__container'));
+  formList.forEach((form) => {
+    const formValidator = new FormValidator(formObjects, form);
+    formValidator.enableValidation();
+  });
+}
+addValidation();
+
+
+
+// *** Слушатели *** //
+// Кнопка подтверждения карточек
+cardsElement.addEventListener('submit', cardSubmitHandler);
+// Кнопки открытия форм
+profileEditButton.addEventListener('click', openPopupProfile);
+addCardButton.addEventListener('click', openPopupCards);
+
+
